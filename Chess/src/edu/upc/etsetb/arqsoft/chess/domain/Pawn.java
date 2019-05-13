8345093fbf5fc96d0078a4ae3d5644aaa32f87fa
Nataliajd13 <49880263+Nataliajd13@users.noreply.github.com>
2019-05-13 12:41:42 +0200

    Pawn isPathFree

--- Chess/src/edu/upc/etsetb/arqsoft/chess/domain/Pawn.java
+++ Chess/src/edu/upc/etsetb/arqsoft/chess/domain/Pawn.java
@@ -5,31 +5,97 @@
  */
 package edu.upc.etsetb.arqsoft.chess.domain;
 
-public class Pawn extends Piece{
+public class Pawn extends Piece {
+
     public boolean firstmove = true;
 
     public Pawn(PieceColor color) {
         super(color);
     }
-    
-    public void isPieceMovement(int f1, int c1, int f2, int c2) throws NoPieceMoveException{
-        if( this.firstmove == true && f2-f1 == 2 && c1 == c2){this.firstmove = false; return;}
-        else if(f2-f1 == 1 && c1 == c2){
-            if( this.firstmove == true){ 
-                this.firstmove = false; 
-            }
-        return; }
-        else if(f2-f1 == 1 && ( c2-c1==1 || c1-c2==1 )){
-            if( this.firstmove == true){ 
-                this.firstmove = false;
-            }
-        return;}  
-        throw new NoPieceMoveException("Invalid pawn movement");            
+
+    @Override
+    public void isPieceMovement(int f1, int c1, int f2, int c2) throws NoPieceMoveException {
+        /*
+    * We assume black Pawn moves in increasing line numbers, whils white pawn moves in decreasing number
+         */
+        color = super.getColor();
+        switch (color) {
+            case BLACK:
+                if (this.firstmove == true && f2 - f1 == 2 && c1 == c2) {
+                    this.firstmove = false;
+                    return;
+                } else if (f2 - f1 == 1 && c1 == c2) {
+                    if (this.firstmove == true) {
+                        this.firstmove = false;
+                    }
+                    return;
+                } else if (f2 - f1 == 1 && (c2 - c1 == 1 || c1 - c2 == 1)) {
+                    if (this.firstmove == true) {
+                        this.firstmove = false;
+                    }
+                    return;
+                }
+                throw new NoPieceMoveException("Invalid pawn movement");
+            case WHITE:
+                if (this.firstmove == true && f2 - f1 == -2 && c1 == c2) {
+                    this.firstmove = false;
+                    return;
+                } else if (f2 - f1 == -1 && c1 == c2) {
+                    if (this.firstmove == true) {
+                        this.firstmove = false;
+                    }
+                    return;
+                } else if (f2 - f1 == -1 && (c2 - c1 == 1 || c1 - c2 == 1)) {
+                    if (this.firstmove == true) {
+                        this.firstmove = false;
+                    }
+                    return;
+                }
+                throw new NoPieceMoveException("Invalid pawn movement");
+        }
     }
 
     @Override
     public void isPathFree(int f1, int c1, int f2, int c2, Board b) throws NoPathFreeException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-}
 
+        color = super.getColor();
+        switch (color) {
+            case BLACK: {
+                if ((f2 - f1 == 2 && c1 == c2)) {
+                    if ((b.getPiece(f2, c2) == null) && (b.getPiece(f2 - 1, c2) == null)) {
+                        return;
+                    }
+                } else if (f2 - f1 == 1 && c1 == c2) {
+                    if (b.getPiece(f2, c2) == null) {
+                        return;
+                    }
+                } else if (f2 - f1 == 1 && (c2 - c1 == 1 || c1 - c2 == 1)) {
+                    if (b.getPiece(f2, c2) != null) {
+                        return;
+                    }
+                }
+                throw new NoPathFreeException("Invalid path");
+            }
+            case WHITE: {
+                if ((f2 - f1 == -2 && c1 == c2)) {
+                    if ((b.getPiece(f2, c2) == null) && (b.getPiece(f2 + 1, c2) == null)) {
+                        return;
+                    }
+                } else if (f2 - f1 == -1 && c1 == c2) {
+                    if (b.getPiece(f2, c2) == null) {
+                        return;
+                    }
+                } else if (f2 - f1 == -1 && (c2 - c1 == 1 || c1 - c2 == 1)) {
+                    if (b.getPiece(f2, c2) != null) {
+                        return;
+                    }
+                }
+                throw new NoPathFreeException("Invalid path");
+            }
+        }
+
+    }
+    //To change body of generated methods, choose Tools | Templates.
+
+
+}
--- Chess/src/edu/upc/etsetb/arqsoft/chess/domain/TestPawn.java
+++ Chess/src/edu/upc/etsetb/arqsoft/chess/domain/TestPawn.java
@@ -45,8 +45,7 @@ public class TestPawn extends TestCase {
     // The methods must be annotated with annotation @Test. For example:
     //
     @Test
-    public void testPawn1_OK() throws Exception {
-        System.out.println("testPawn1_OK()");
+    public void testPawn1Black_OK() throws Exception {
         Pawn p = new Pawn(PieceColor.BLACK);
         //first move check, constructor initializes at firstmove = true
         try {
@@ -56,8 +55,7 @@ public class TestPawn extends TestCase {
             Assert.assertTrue(false);
         }
     }
-    public void testPawn2_OK() throws Exception {
-        System.out.println("testPawn1_OK()");
+    public void testPawn2Black_OK() throws Exception {
         Pawn p = new Pawn(PieceColor.BLACK);
         // Kill to the front right
         try {
@@ -67,8 +65,7 @@ public class TestPawn extends TestCase {
             Assert.assertTrue(false);
         }
     }
-    public void testPawn3_OK() throws Exception {
-        System.out.println("testPawn1_OK()");
+    public void testPawn3Black_OK() throws Exception {
         Pawn p = new Pawn(PieceColor.BLACK);
         // Kill to the front left
         try {
@@ -79,7 +76,7 @@ public class TestPawn extends TestCase {
         }
     }
 
-    public void testPawn4_OK() throws Exception {
+    public void testPawn4Black_OK() throws Exception {
         Pawn p = new Pawn(PieceColor.BLACK);
         // Move one to the front
         try {
@@ -90,8 +87,7 @@ public class TestPawn extends TestCase {
         }
     }
     
-    public void testPawn1_KO() throws Exception {
-        System.out.println("testPawn1_KO()");
+    public void testPawn1Black_KO() throws Exception {
         Pawn p = new Pawn(PieceColor.BLACK);
         p.isPieceMovement(1, 4, 3, 4);
         //we make a move before test, first move will be false, and we try to make to at once
@@ -103,8 +99,7 @@ public class TestPawn extends TestCase {
         }
     }
 
-    public void testPawn2_KO() throws Exception {
-        System.out.println("testPawn1_OK()");
+    public void testPawn2Black_KO() throws Exception {
         Pawn p = new Pawn(PieceColor.BLACK);
         // We only move to the right, not straight
         try {
@@ -115,8 +110,7 @@ public class TestPawn extends TestCase {
         }
     }
     
-    public void testPawn3_KO() throws Exception {
-        System.out.println("testPawn1_OK()");
+    public void testPawn3Black_KO() throws Exception {
         Pawn p = new Pawn(PieceColor.BLACK);
         // We only move to the left, not straight
         try {
@@ -126,8 +120,7 @@ public class TestPawn extends TestCase {
             Assert.assertTrue(true);
         }
     }
-    public void testPawn4_KO() throws Exception {
-        System.out.println("testPawn1_OK()");
+    public void testPawn4Black_KO() throws Exception {
         Pawn p = new Pawn(PieceColor.BLACK);
         // We move backwards
         try {
@@ -138,22 +131,22 @@ public class TestPawn extends TestCase {
         }
     }
         
-    public void testPawn5_KO() throws Exception {
-        Pawn p = new Pawn(PieceColor.BLACK);
+    public void testPawn5Black_KO() throws Exception {
+        Pawn p = new Pawn(PieceColor.WHITE);
         // Kill back left
         try {
-            p.isPieceMovement(2, 3, 1, 2);
+            p.isPieceMovement(1, 3, 2, 2);
             Assert.assertTrue(false);
         } catch( NoPieceMoveException e ){
             Assert.assertTrue(true);
         }
     }
 
-    public void testPawn6_KO() throws Exception {
-        Pawn p = new Pawn(PieceColor.BLACK);
+    public void testPawn6Black_KO() throws Exception {
+        Pawn p = new Pawn(PieceColor.WHITE);
         // We Kill back right
         try {
-            p.isPieceMovement(2, 3, 1, 4);
+            p.isPieceMovement(1, 3, 2, 4);
             Assert.assertTrue(false);
         } catch( NoPieceMoveException e ){
             Assert.assertTrue(true);
@@ -161,20 +154,289 @@ public class TestPawn extends TestCase {
     }
 
 
+        public void testPawn1White_OK() throws Exception {
+        Pawn p = new Pawn(PieceColor.WHITE);
+        //first move check, constructor initializes at firstmove = true
+        try {
+            p.isPieceMovement(3, 4, 1, 4);
+            Assert.assertTrue(true);
+        } catch( NoPieceMoveException e ){
+            Assert.assertTrue(false);
+        }
+    }
+    public void testPawn2White_OK() throws Exception {
+        Pawn p = new Pawn(PieceColor.WHITE);
+        // Kill to the front right
+        try {
+            p.isPieceMovement(3, 3, 2, 4);
+            Assert.assertTrue(true);
+        } catch( NoPieceMoveException e ){
+            Assert.assertTrue(false);
+        }
+    }
+    public void testPawn3White_OK() throws Exception {
+        Pawn p = new Pawn(PieceColor.WHITE);
+        // Kill to the front left
+        try {
+            p.isPieceMovement(3, 3, 2, 2);
+            Assert.assertTrue(true);
+        } catch( NoPieceMoveException e ){
+            Assert.assertTrue(false);
+        }
+    }
+
+    public void testPawn4White_OK() throws Exception {
+        Pawn p = new Pawn(PieceColor.WHITE);
+        // Move one to the front
+        try {
+            p.isPieceMovement(3, 3, 2, 3);
+            Assert.assertTrue(true);
+        } catch( NoPieceMoveException e ){
+            Assert.assertTrue(false);
+        }
+    }
+    
+    public void testPawn1White_KO() throws Exception {
+        Pawn p = new Pawn(PieceColor.WHITE);
+        p.isPieceMovement(3, 4, 2, 4);
+        //we make a move before test, first move will be false, and we try to make to at once
+        try {
+            p.isPieceMovement(6, 4, 3, 4);
+            Assert.assertTrue(false);
+        } catch( NoPieceMoveException e ){
+            Assert.assertTrue(true);
+        }
+    }
+
+    public void testPawn2White_KO() throws Exception {
+        Pawn p = new Pawn(PieceColor.WHITE);
+        // We only move to the right, not straight
+        try {
+            p.isPieceMovement(3, 4, 3, 5);
+            Assert.assertTrue(false);
+        } catch( NoPieceMoveException e ){
+            Assert.assertTrue(true);
+        }
+    }
+    
+    public void testPawn3White_KO() throws Exception {
+        Pawn p = new Pawn(PieceColor.WHITE);
+        // We only move to the left, not straight
+        try {
+            p.isPieceMovement(2, 4, 2, 3);
+            Assert.assertTrue(false);
+        } catch( NoPieceMoveException e ){
+            Assert.assertTrue(true);
+        }
+    }
+    public void testPawn4White_KO() throws Exception {
+        Pawn p = new Pawn(PieceColor.WHITE);
+        // We move backwards
+        try {
+            p.isPieceMovement(1, 3, 2, 3);
+            Assert.assertTrue(false);
+        } catch( NoPieceMoveException e ){
+            Assert.assertTrue(true);
+        }
+    }
+        
+    public void testPawn5White_KO() throws Exception {
+        Pawn p = new Pawn(PieceColor.WHITE);
+        // Kill back left
+        try {
+            p.isPieceMovement(1, 3, 2, 2);
+            Assert.assertTrue(false);
+        } catch( NoPieceMoveException e ){
+            Assert.assertTrue(true);
+        }
+    }
+
+    public void testPawn6White_KO() throws Exception {
+        Pawn p = new Pawn(PieceColor.WHITE);
+        // We Kill back right
+        try {
+            p.isPieceMovement(1, 3, 2, 4);
+            Assert.assertTrue(false);
+        } catch( NoPieceMoveException e ){
+            Assert.assertTrue(true);
+        }
+    }
+        
+    public void testPawnPath1Black_KO() throws Exception {
+        Pawn p = new Pawn(PieceColor.BLACK);
+        Board b = new Board(true);
+        Piece p1 = b.getPiece(0,0);
+        b.movePiece(0, 0, 3, 0, p1);
+        // There is a rock in [0][0]
+        try {
+            p.isPathFree(1, 0, 3, 0, b);
+            Assert.assertTrue(false);
+        } catch( NoPathFreeException e ){
+            Assert.assertTrue(true);
+        }    
+    }
+    
+    public void testPawnPath1Black_OK() throws Exception {
+        Pawn p = new Pawn(PieceColor.BLACK);
+        Board b = new Board(true);
+        Piece p1 = b.getPiece(0,0);
+        b.movePiece(0, 0, 3, 0, p1);
+        // There is a black rock in [0][0]
+        try {
+            p.isPathFree(1, 0, 2, 0, b);
+            Assert.assertTrue(true);
+        } catch( NoPathFreeException e ){
+            Assert.assertTrue(false);
+        }    
+    }
+    
+    public void testPawnPath1White_KO() throws Exception {
+        Pawn p = new Pawn(PieceColor.WHITE);
+        Board b = new Board(true);
+        Piece p1 = b.getPiece(7,0);
+        b.movePiece(7, 0, 4, 0, p1);
+        // There is a black rock in [0][0]
+        try {
+            p.isPathFree(6, 0, 4, 0, b);
+            Assert.assertTrue(false);
+        } catch( NoPathFreeException e ){
+            Assert.assertTrue(true);
+        }    
+    }    
+    
+    public void testPawnPath3White_KO() throws Exception {
+        Pawn p = new Pawn(PieceColor.WHITE);
+        Board b = new Board(true);
+        Piece p1 = b.getPiece(7,0);
+        b.movePiece(7, 0, 5, 0, p1);
+        // There is a black rock in [0][0]
+        try {
+            p.isPathFree(6, 0, 4, 0, b);
+            Assert.assertTrue(false);
+        } catch( NoPathFreeException e ){
+            Assert.assertTrue(true);
+        }    
+    }   
+    
+    public void testPawnPath3Black_KO() throws Exception {
+        Pawn p = new Pawn(PieceColor.BLACK);
+        Board b = new Board(true);
+        Piece p1 = b.getPiece(0,0);
+        b.movePiece(0, 0, 2, 0, p1);
+        // There is a black rock in [0][0]
+        try {
+            p.isPathFree(1, 0, 3, 0, b);
+            Assert.assertTrue(false);
+        } catch( NoPathFreeException e ){
+            Assert.assertTrue(true);
+        }    
+    }   
+    
+    public void testPawnPath1White_OK() throws Exception {
+        Pawn p = new Pawn(PieceColor.WHITE);
+        Board b = new Board(true);
+        Piece p1 = b.getPiece(7,0);
+        b.movePiece(7, 0, 4, 0, p1);
+        // There is a black rock in [0][0]
+        try {
+            p.isPathFree(6, 0, 5, 0, b);
+            Assert.assertTrue(true);
+        } catch( NoPathFreeException e ){
+            Assert.assertTrue(false);
+        }    
+    }
+    
+    public void testPawnPath2Black_OK() throws Exception {
+        Pawn p = new Pawn(PieceColor.BLACK);
+        Board b = new Board(true);
+        Piece p1 = b.getPiece(6,0);
+        b.movePiece(6, 0, 4, 0, p1);
+        // There is a white pawn in [6][0]
+        try {
+            p.isPathFree(3, 1, 4, 0, b);
+            Assert.assertTrue(true);
+        } catch( NoPathFreeException e ){
+            Assert.assertTrue(false);
+        }    
+    }    
+
+    public void testPawnPath2Black_KO() throws Exception {
+        Pawn p = new Pawn(PieceColor.BLACK);
+        Board b = new Board(true);
+        // There is a nothing in [4][0]
+        try {
+            p.isPathFree(3, 1, 4, 0, b);
+            Assert.assertTrue(false);
+        } catch( NoPathFreeException e ){
+            Assert.assertTrue(true);
+        }    
+    }
+    
+    public void testPawnPath2White_OK() throws Exception {
+        Pawn p = new Pawn(PieceColor.WHITE);
+        Board b = new Board(true);
+        Piece p1 = b.getPiece(1,0);
+        b.movePiece(1, 0, 3, 0, p1);
+        // There is a black pawn in [1][0]
+        try {
+            p.isPathFree(4, 1, 3, 0, b);
+            Assert.assertTrue(true);
+        } catch( NoPathFreeException e ){
+            Assert.assertTrue(false);
+        }    
+    }
+    
+    public void testPawnPath2White_KO() throws Exception {
+        Pawn p = new Pawn(PieceColor.WHITE);
+        Board b = new Board(true);
+        // There is a nothing in [5][0]
+        try {
+            p.isPathFree(6, 1, 5, 0, b);
+            Assert.assertTrue(false);
+        } catch( NoPathFreeException e ){
+            Assert.assertTrue(true);
+        }    
+    }
     
     public static TestSuite suite() {
         TestSuite suite = new TestSuite();
-        suite.addTest(new TestPawn("testPawn1_OK"));
-        suite.addTest(new TestPawn("testPawn2_OK"));
-        suite.addTest(new TestPawn("testPawn3_OK"));
-        suite.addTest(new TestPawn("testPawn4_OK"));
+        suite.addTest(new TestPawn("testPawn1Black_OK"));
+        suite.addTest(new TestPawn("testPawn2Black_OK"));
+        suite.addTest(new TestPawn("testPawn3Black_OK"));
+        suite.addTest(new TestPawn("testPawn4Black_OK"));
         
-        suite.addTest(new TestPawn("testPawn1_KO"));
-        suite.addTest(new TestPawn("testPawn2_KO"));
-        suite.addTest(new TestPawn("testPawn3_KO"));
-        suite.addTest(new TestPawn("testPawn4_KO"));
-        suite.addTest(new TestPawn("testPawn5_KO"));
-        suite.addTest(new TestPawn("testPawn6_KO"));
+        suite.addTest(new TestPawn("testPawn1Black_KO"));
+        suite.addTest(new TestPawn("testPawn2Black_KO"));
+        suite.addTest(new TestPawn("testPawn3Black_KO"));
+        suite.addTest(new TestPawn("testPawn4Black_KO"));
+        suite.addTest(new TestPawn("testPawn5Black_KO"));
+        suite.addTest(new TestPawn("testPawn6Black_KO"));
+
+        suite.addTest(new TestPawn("testPawn1White_OK"));
+        suite.addTest(new TestPawn("testPawn2White_OK"));
+        suite.addTest(new TestPawn("testPawn3White_OK"));
+        suite.addTest(new TestPawn("testPawn4White_OK"));
+        
+        suite.addTest(new TestPawn("testPawn1White_KO"));
+        suite.addTest(new TestPawn("testPawn2White_KO"));
+        suite.addTest(new TestPawn("testPawn3White_KO"));
+        suite.addTest(new TestPawn("testPawn4White_KO"));
+        suite.addTest(new TestPawn("testPawn5White_KO"));
+        suite.addTest(new TestPawn("testPawn6White_KO"));
+        
+        suite.addTest(new TestPawn("testPawnPath1Black_KO"));
+        suite.addTest(new TestPawn("testPawnPath1Black_OK"));
+        suite.addTest(new TestPawn("testPawnPath2Black_KO"));
+        suite.addTest(new TestPawn("testPawnPath2Black_KO"));
+        suite.addTest(new TestPawn("testPawnPath3Black_KO"));
+        
+        suite.addTest(new TestPawn("testPawnPath1White_KO"));
+        suite.addTest(new TestPawn("testPawnPath1White_OK"));
+        suite.addTest(new TestPawn("testPawnPath2White_KO"));
+        suite.addTest(new TestPawn("testPawnPath2White_OK"));
+        suite.addTest(new TestPawn("testPawnPath3White_KO"));
+        
+        
         return suite;
     }
 }
